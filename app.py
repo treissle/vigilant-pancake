78cb1204fc2020fc47f1fa313441a3625b288656
treissle <157082749+treissle@users.noreply.github.com>
2024-03-13 19:42:37 +0000

    Login route

--- app.py
+++ app.py
@@ -11,3 +11,38 @@ app.config['MYSQL_USER'] = 'spring2024Cteam10'
 app.config['MYSQL_PASSWORD'] = 'spring2024Cteam10'
 app.config['MYSQL_DB'] = 'spring2024Cteam10'
 mysql = MySQL(app)
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM XXXXXXX WHERE username = %s AND password = %s', (username, password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            session['type'] = account['type']
+      
+            session['user_data'] = {
+                'XXXX': account['XXXX'],
+                'XXXX': account['XXXX']
+            }
+            
+            if session['type'] == 'mentor':
+                return redirect(url_for('mentor'))
+            elif session['type'] == 'mentee'
+                return redirect(url_for('mentee'))
+            
+        else:
+            msg = 'Incorrect username/password!'
+            return render_template('login.html', msg=msg)
+    return render_template('login.html', msg='')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
