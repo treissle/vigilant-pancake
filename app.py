e8d579a84136dd2975fd5918c96a43ab67d29421
treissle <157082749+treissle@users.noreply.github.com>
2024-03-25 19:31:46 +0000

    Update SQL code

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def register():
         if account:
             msg = 'Account already exists !'
         else:
-            cursor.execute('INSERT INTO GUIDEU VALUES (NULL, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s)', (username, password, usertype, firstname, lastname, bio, department, certification, IorE, TorS, MorS))
+            cursor.execute('INSERT INTO GUIDEU VALUES (NULL, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s)', (username, password, usertype, bio, department, certification, IorE, TorS, MorS, firstname, lastname))
             mysql.connection.commit()
             msg = 'You have successfully registered'
             return render_template('login.html', msg = msg, username=username)
